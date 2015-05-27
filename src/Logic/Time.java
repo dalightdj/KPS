8b26e70c70ea416a9abaf90d2fc0d46ffd62b433
josh2g <joshuanhood@gmail.com>
2015-05-27 22:52:29 +1200

    Added Initial logic from computer, including LDC, KPS, Delivery, MailRecord, Journey. Needs a lot of main methods in KPS to be done

---
+++ src/Logic/Time.java
@@ -0,0 +1,13 @@
+package Logic;
+
+public class Time {
+	private int seconds;
+	private int hours;
+	
+	public int getSeconds(){
+		return seconds;
+	}
+	public int getHours(){
+		return hours;
+	}
+}
