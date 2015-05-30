a19f189c2d092c25d8c8f4d6b4592eb7aeff69f3
JoshuaHood <joshuaNHood@gmail.com>
2015-05-30 16:41:49 +1200

    Added KPS constructor, and started an update events method

--- src/Logic/KPS.java
+++ src/Logic/KPS.java
@@ -2,17 +2,30 @@ package Logic;
 
 import java.util.ArrayList;
 
+import Logger.Event;
+import Logger.Logger;
 import Logger.MDEvent;
 
 public class KPS {
+
 	private double revenueTotal;
 	private double expendetureTotal;
 	private int deliveriesCount;
 	private ArrayList<LocalDistributionCentre> LDCs;
-	//private ArrayList<Event> events;
+	private ArrayList<Event> events;
 	private ArrayList<Journey> journeys;
+	private Logger logger;
 	//private graph sructure???
-	//XML object ?? 
+
+	public KPS(){
+		revenueTotal = 0.0;
+		expendetureTotal = 0.0;
+		deliveriesCount = 0;
+		LDCs = new ArrayList<LocalDistributionCentre>();
+		journeys = new ArrayList<Journey>();
+		logger = new Logger();
+		loadEvents(); //WILL BE NULL AT THE START UNLESS WE HAVE A DEFAULT XML FILE
+	}
 
 	public void discontinueRoute(int reference){
 		//XML event
@@ -20,10 +33,21 @@ public class KPS {
 		// take out path from the GRAPH STRUCTURE with the given reference ?
 	}
 
+	/**
+	 *
+	 * Deals with a mail Delivery event appropriately (and creates the new MDEvent object
+	 * if given a createNew boolean as true
+	 *
+	 * @param createNew Is this a new mailDelivery event being entered from the GUI. (always true for Ewan/Neal)
+	 */
+	public void mailDelivery(String date, String destination, String origin, int weight, int volume, String priority, boolean createNew){
 
-	public void mailDelivery(String date, String destination, String origin, int weight, int volume, String priority){
-
-		MDEvent event = new MDEvent(date, destination, origin, priority, weight, volume);
+		//Create an event object, add it to the arrayList and write it in XML using the Logger.
+		if(createNew){
+			MDEvent event = new MDEvent(date, destination, origin, priority, weight, volume);
+			events.add(event);
+			logger.addEvent(event);
+		}
 
 		//Update appropriate MailRecord
 		boolean recordFound = false;
@@ -44,14 +68,28 @@ public class KPS {
 					MailRecord rec = new MailRecord(destination);
 					rec.incrementCount();rec.incrementVolume(volume);rec.incrementWeight(weight);
 					ldc.getAllRecords().add(rec);
-				}	
+				}
 			}
 		}
 
-		//write XML
-		//make boolean ? 
+		//TODO: make boolean ?
 
-	} 
+	}
+
+	public void loadEvents(){
+		if(logger.readXML() == null){
+			return;
+		}
+		events = logger.readXML();
+		for (Event e:events){
+			if(e instanceof MDEvent ){
+				mailDelivery(((MDEvent) e).getDate(), ((MDEvent) e).getDestination(), ((MDEvent) e).getOrigin(),
+						((MDEvent) e).getWeight(), ((MDEvent) e).getVolume(), ((MDEvent) e).getPriority(), false);
+			}
+		}
+
+
+	}
 
 
 	public void priceUpdate(){
