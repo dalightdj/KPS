5b4139e8b5f194d3b358b423d5eb939dd8095ad4
JoshuaHood <joshuaNHood@gmail.com>
2015-05-30 20:28:57 +1200

    worked on transport cost update handling, added getDay(), changed String priorities to enums. also added back the types which are different to priority (which I didn't know before)

--- src/Logic/KPS.java
+++ src/Logic/KPS.java
@@ -2,9 +2,14 @@ package Logic;
 
 import java.util.ArrayList;
 
+import travelGraph.Path.DayOfWeek;
+import travelGraph.Path.TransportType;
+import travelGraph.TravelGraph;
+import travelGraph.TravelGraph.Priority;
 import Logger.Event;
 import Logger.Logger;
 import Logger.MDEvent;
+import Logger.TCUEvent;
 
 public class KPS {
 
@@ -15,6 +20,7 @@ public class KPS {
 	private ArrayList<Event> events;
 	private ArrayList<Journey> journeys;
 	private Logger logger;
+	private TravelGraph travelGraph;
 	//private graph sructure???
 
 	public KPS(){
@@ -26,6 +32,7 @@ public class KPS {
 		journeys = new ArrayList<Journey>();
 		logger = new Logger(); //Does not affect the XML file.
 		loadEvents(); //WILL BE NULL AT THE START UNLESS WE HAVE A DEFAULT XML FILE
+		travelGraph = new TravelGraph();
 	}
 
 	public void discontinueRoute(int reference){
@@ -41,7 +48,7 @@ public class KPS {
 	 *
 	 * @param createNew Is this a new mailDelivery event being entered from the GUI. (always true for Ewan/Neal)
 	 */
-	public void mailDelivery(String date, String destination, String origin, int weight, int volume, String priority, boolean createNew){
+	public void mailDelivery(String date, String destination, String origin, int weight, int volume, Priority priority, boolean createNew){
 
 		//Create an event object, add it to the arrayList and write it in XML using the Logger.
 		if(createNew){
@@ -49,17 +56,13 @@ public class KPS {
 			events.add(event);
 			logger.addEvent(event);
 		}
-
 		//Update appropriate MailRecord
 		boolean recordFound = false;
 		for(LocalDistributionCentre ldc:LDCs){
 			if(ldc.getName().equals(origin)){
-				for(MailRecord mr:ldc.getAllRecords()){
-					if(mr.getDestination().equals(destination)){
-						recordFound = true;
-						mr.incrementCount();mr.incrementVolume(volume);mr.incrementWeight(weight);
-					}
-				}
+				MailRecord mr = ldc.getRecord(destination);
+				mr.incrementCount();mr.incrementVolume(volume);mr.incrementWeight(weight);
+				recordFound = true;
 			}
 		}
 		//Or create a MailRecord if one doesn't exist for this journey
@@ -72,9 +75,7 @@ public class KPS {
 				}
 			}
 		}
-
 		//TODO: make boolean ?
-
 	}
 
 	public void loadEvents(){
@@ -87,9 +88,8 @@ public class KPS {
 				mailDelivery(((MDEvent) e).getDate(), ((MDEvent) e).getDestination(), ((MDEvent) e).getOrigin(),
 						((MDEvent) e).getWeight(), ((MDEvent) e).getVolume(), ((MDEvent) e).getPriority(), false);
 			}
+
 		}
-
-
 	}
 
 
@@ -99,10 +99,58 @@ public class KPS {
 		//change in the GRAPH STRUCTURE
 	}
 
-	public void costUpdate(){
+	public void costUpdate(String company, String destination, String origin, TransportType type, String date,
+			int weightCost, int volumeCost, int maxWeight, int maxVolume, int duration, int frequency, boolean createNew){
+
+		//Create an event object, add it to the arrayList and write it in XML using the Logger.
+		if(createNew){
+			TCUEvent event = new TCUEvent(company, destination, origin, type, date,
+					weightCost, volumeCost, maxWeight, maxVolume, duration, frequency);
+			events.add(event);
+			logger.addEvent(event);
+		}
+
+		travelGraph.updatePathPrice(origin, destination, company, priority, weightCost, volumeCost, getDay(date), frequency, duration);
+
+
+
+
 		//write XML
 		//make boolean
 		//update JOURNEY ? ??
+	}
+
+	/**Takes in the date String of format "EEE HH:mm" and returns the day as type enum DayOfWeek.
+	 *
+	 * @param date, the string in the form "EEE HH:mm" where EEE is the name of the day (first 3 characters)
+	 * HH is hours and mm is minutes. Only concerned about the name of the day here.
+	 *
+	 * @return enum DayOfWeek
+	 */
+	private DayOfWeek getDay(String date) {
+		// TODO Auto-generated method stub
+		String day = date.substring(0, 3);
+		if(day.equalsIgnoreCase("Mon")){
+			return DayOfWeek.MONDAY;
+		}
+		else if(day.equalsIgnoreCase("Tue")){
+			return DayOfWeek.TUESDAY;
+		}
+		else if(day.equalsIgnoreCase("Wed")){
+			return DayOfWeek.WEDNESDAY;
+		}
+		else if(day.equalsIgnoreCase("Thu")){
+			return DayOfWeek.THURSDAY;
+		}
+		else if(day.equalsIgnoreCase("Fri")){
+			return DayOfWeek.FRIDAY;
+		}
+		else if(day.equalsIgnoreCase("Sat")){
+			return DayOfWeek.SATURDAY;
+		}
+		else {
+			return DayOfWeek.SUNDAY;
+		}
 	}
 
 
