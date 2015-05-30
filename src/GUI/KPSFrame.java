4e549f022789b1fc0a2e79c8f33aa8e285d7fead
EwanMoshi <ewanmoshi@live.com>
2015-05-30 20:42:54 +1200

    added gui info labels on the main frame

--- src/GUI/KPSFrame.java
+++ src/GUI/KPSFrame.java
@@ -4,7 +4,10 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.image.BufferedImage;
 
 import javax.swing.BorderFactory;
@@ -26,6 +29,24 @@ public class KPSFrame extends JFrame {
 	
 	private BufferedImage frameIcon;
 
+	/*Labels for the GUI information*/
+	private JLabel totalRevenue;
+	private JLabel totalExpenditure;
+	private JLabel totalNumberOfEvents;
+	private JLabel amountOfMail;
+	private JLabel averageDeliveryTime;
+	private JLabel criticalRoutes;
+
+	/*Labels that contain the values*/
+	private JLabel totalRevenueValue;
+	private JLabel totalExpenditureValue;
+	private JLabel totalNumberOfEventsValue;
+	private JLabel amountOfMailValue;
+	private JLabel averageDeliveryTimeValue;
+	private JLabel criticalRoutesValue;
+
+	
+	
 	public KPSFrame() {
 		
 		/*Initialize the layout and the insets*/
@@ -41,9 +62,12 @@ public class KPSFrame extends JFrame {
  		this.add(buttonsPanel, BorderLayout.WEST);
 		
  		guiInformation = new JPanel();
+ 		guiInformation.setLayout(new GridBagLayout());
  		guiInformation.setBorder(BorderFactory.createLineBorder(Color.red));
  		this.add(guiInformation,BorderLayout.CENTER);
- 				
+ 			
+ 		setupGuiInformationLabels();
+ 		
 		this.setPreferredSize(new Dimension(1000,800));
 		buttonsPanel.setPreferredSize(new Dimension(220,this.getHeight()));
 		this.setResizable(true);
@@ -53,6 +77,57 @@ public class KPSFrame extends JFrame {
 		JOptionPane.showMessageDialog(this, "Welcome to Kelburn Postal Service!", "Welcome", JOptionPane.INFORMATION_MESSAGE); //Welcome the user (maybe remove this as it can be annoying)
 	}
 	
+	private void setupGuiInformationLabels() {
+		GridBagConstraints c = new GridBagConstraints();
+		c.insets = new Insets(0,-50,30,50); //top, left, bottom, right padding (in that order)
+		c.fill = GridBagConstraints.HORIZONTAL;
+		
+		totalRevenue = new JLabel("Total Revenue: ");
+		totalRevenue.setBorder(BorderFactory.createLineBorder(Color.red));
+		
+		totalExpenditure = new JLabel("Total Expenditure: ");
+		totalExpenditure.setBorder(BorderFactory.createLineBorder(Color.red));
+
+		totalNumberOfEvents = new JLabel("Total Number of Events: ");
+		totalNumberOfEvents.setBorder(BorderFactory.createLineBorder(Color.red));
+		
+		amountOfMail = new JLabel("Amount of Mail: ");
+		amountOfMail.setBorder(BorderFactory.createLineBorder(Color.red));
+		
+		averageDeliveryTime = new JLabel("Average Delivery Time: ");
+		averageDeliveryTime.setBorder(BorderFactory.createLineBorder(Color.red));
+		
+		criticalRoutes = new JLabel("Critical Routes: ");
+		criticalRoutes.setBorder(BorderFactory.createLineBorder(Color.red));			
+		
+		c.weightx = 1.0;
+		
+		c.gridx = 0;
+		c.gridy = 0;
+		guiInformation.add(totalRevenue,c);
+		
+		c.gridx = 0;
+		c.gridy = 1;
+		guiInformation.add(totalExpenditure,c);
+		
+		c.gridx = 0;
+		c.gridy = 2;
+		guiInformation.add(totalNumberOfEvents,c);
+		
+		c.gridx = 0;
+		c.gridy = 3;
+		guiInformation.add(amountOfMail,c);
+		
+		c.gridx = 0;
+		c.gridy = 4;
+		guiInformation.add(averageDeliveryTime,c);
+		
+		c.gridx = 0;
+		c.gridy = 5;
+		guiInformation.add(criticalRoutes,c);
+				
+	}
+
 	/**
 	 * This is just a test method to run it directly
 	 * @param args
