d99a3e0530f8a9562db1960af2e433dd8dac154d
josh2g <joshuanhood@gmail.com>
2015-05-30 13:24:38 +1200

    Delete delivery class, update logic
    Delivery class doesn't need to be there. Journey updated appropriately,
    KPS, mail delivery method added, LDC now extends Location.

--- src/Logic/Journey.java
+++ src/Logic/Journey.java
@@ -1,26 +1,32 @@
 package Logic;
 
+import java.util.ArrayList;
+
+import travelGraph.Path;
+
 public class Journey {
 	private String destination;
 	private String origin;
 	private int priority;
-	private double averageDeliveryTime;
-	private double averageDeliveryCost;
-	private double averagePrice;
-	private double deliveryTimeSum;
-	private double deliveryCostSum;
-	private double priceSum;
-	private int count;
+	private Double weightPrice;
+	private Double volumePrice;
+	private ArrayList<Path> usedPaths;
+//	private double averageDeliveryTime;
+//	private double averagePrice;
+//	private double deliveryTimeSum;
+//	private double priceSum;
+//	private int count;
 
-	public Journey(String destination, String origin, int priority,
-			double averageDeliveryTime, double averageDeliveryCost,
-			double averagePrice) {
+	public Journey(String destination, String origin, int priority, 
+			double weightPrice, double volumePrice, ArrayList<Path> usedPaths) {
 		this.destination = destination;
 		this.origin = origin;
 		this.priority = priority;
-		this.averageDeliveryTime = averageDeliveryTime;
-		this.averageDeliveryCost = averageDeliveryCost;
-		this.averagePrice = averagePrice;
+		this.weightPrice = weightPrice;
+		this.volumePrice = volumePrice;
+		this.usedPaths = usedPaths;
+//		this.averageDeliveryTime = averageDeliveryTime;
+//		this.averagePrice = averagePrice;
 	}
 
 	public String getDestination() {
@@ -32,13 +38,11 @@ public class Journey {
 	public int getPriority() {
 		return priority;
 	}
-	public double getAverageDeliveryTime() {
-		return averageDeliveryTime;
-	}
-	public double getAverageDeliveryCost() {
-		return averageDeliveryCost;
-	}
-	public double getAveragePrice() {
-		return averagePrice;
-	}
+//	public double getAverageDeliveryTime() {
+//		return averageDeliveryTime;
+//	}
+//	public double getAveragePrice() {
+//		return averagePrice;
+//	}
+
 }
