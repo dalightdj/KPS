16b66cb903ca380fd0ac87dc19b4ed8dc41ed843
dalightdj <dalightdj@yahoo.com.au>
2015-06-02 11:23:04 +1200

    Add jUnit package

---
+++ src/jUnit/travelGraph_Test.java
@@ -0,0 +1,14 @@
+package jUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class travelGraph_Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
